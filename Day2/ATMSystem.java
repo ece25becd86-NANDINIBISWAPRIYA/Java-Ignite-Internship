c2093bec46e9e6dc51b4859171edef50c89f7900
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-05-27 15:09:07 +0530

    Add files via upload

---
+++ Day2/ATMSystem.java
@@ -0,0 +1,20 @@
+package JavaIgniteDay2;
+import java.util.Scanner;
+
+public class ATMSystem {
+
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter your name:");
+			String name = sc.nextLine();
+			System.out.println("Account balance (in Rs.):");
+			double balance=sc.nextDouble();
+			System.out.println("Withdrawl amount (in Rs.):");
+			double withdraw=sc.nextDouble();
+			System.out.println("Account holder "+name+"'s bank statement:");
+			System.out.println("Remaining balance: Rs."+(balance-withdraw));
+			System.out.println("Is balance greater than withdraw: "+(balance>withdraw));
+			sc.close();
+		}
+
+	}
