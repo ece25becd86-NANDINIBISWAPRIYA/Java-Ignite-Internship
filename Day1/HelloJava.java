fc70222c020dcfbfe8d744df477c5a38257e0646
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-05-27 10:32:40 +0530

    Add files via upload

---
+++ Day1/HelloJava.java
@@ -0,0 +1,10 @@
+package JavaIgniteDay1;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Welcome to Java Ignite Internship Program");
+		System.out.println("Hello Java");
+	}
+
+}
